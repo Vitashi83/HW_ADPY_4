9864c04cff8596cb11787f2c85df9630f07d74db
Vitalii <91472580+Vitashi83@users.noreply.github.com>
2022-01-26 08:41:22 +0400

    Update HW_ADPY_4_iter.py

--- HW_ADPY_4_iter.py
+++ HW_ADPY_4_iter.py
@@ -1,21 +1,23 @@
 class FlatIterator:
 
-    def __init__(self, nested_list):
-        self.list = - 1
-        self.end = len(nested_list)
-       
-
+    def __init__(self, some_list):
+        self.main_list = some_list
+        
     def __iter__(self):
+        self.main_list_cursor = 0  # курсор основного списка
+        self.nested_list_cursor = -1 # курсор списка вложенного в основной список
         return self
 
-
     def __next__(self):
-        self.list += 1
-        if self.list == self.end:
+        self.nested_list_cursor += 1    # увеличиваем nested_list_cursor
+        if self.nested_list_cursor == len(self.main_list[self.main_list_cursor]): # если во вложенном списке элементы закончились,
+            self.main_list_cursor +=1  # то переходи на следующий список увеличив main_list_cursor
+            self.nested_list_cursor = 0 # и обнуляем main_list_cursor
+        if self.main_list_cursor == len(self.main_list):
             raise StopIteration
-        return '\n'.join(str(item) for item in nested_list[self.list])
+        return self.main_list[self.main_list_cursor][self.nested_list_cursor]
 
-    
+
 nested_list = [
 	['a', 'b', 'c'],
 	['d', 'e', 'f', 'h', False],
@@ -23,6 +25,8 @@ nested_list = [
 ]
 
 
-if __name__ == '__main__':
-    for item in FlatIterator(nested_list):
-	    print(item)   
+for item in FlatIterator(nested_list):
+	print(item)
+
+flat_list = [item for item in FlatIterator(nested_list)]
+print(flat_list)
